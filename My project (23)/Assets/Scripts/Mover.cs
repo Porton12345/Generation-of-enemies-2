441a18d9907b5af04e839a74996f58c91006f9d0
Porton12345 <vkruk123@mail.ru>
2024-02-26 16:35:15 +0300

    7 Commit

--- My project (23)/Assets/Scripts/Mover.cs
+++ My project (23)/Assets/Scripts/Mover.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {   
-    [SerializeField, Min(0)] private float _speed;      
+    [SerializeField, Min(0)] private float _speed;
+    
+    public Quaternion SetDirection(Vector3 direction)
+    {
+        return Quaternion.LookRotation(direction);
+    }
 
     private void Update()
     {
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-    }   
+    }      
 }
--- My project (23)/Assets/Scripts/Spawner.cs
+++ My project (23)/Assets/Scripts/Spawner.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Vector3 _direction;
+    [SerializeField] private Mesh _mesh;
     [SerializeField] private Transform[] _spawnPoints;
-    [SerializeField] private GameObject _enemyCapsule;
+    [SerializeField] private Enemy _enemy;
     [SerializeField, Min(0)] private float _countSecondsBeforeSpawp;
     [SerializeField] private Material _material;
 
     private Vector3 _position;
     private int minRandom = 0;
     private int maxRandom = 5;
+    private int minRandomDegree = -100;
+    private int maxRandomDegree = 100;
     private int _currentSpawnpoint = 0;
 
     private void Start()
@@ -22,12 +25,13 @@ public class Spawner : MonoBehaviour
     }
 
     private void CreateWithInstantiate()
-    {
+    {           
         _currentSpawnpoint = Random.Range(minRandom, maxRandom);
-        _position = _spawnPoints[_currentSpawnpoint].position;
-        Vector3 direction = new Vector3(Random.Range(minRandom, maxRandom), 0, Random.Range(minRandom, maxRandom));
-        var enemy = Instantiate(_enemyCapsule, _position, Quaternion.LookRotation(_direction));
+        _position = _spawnPoints[_currentSpawnpoint].position;        
+        Vector3 direction = new Vector3(Random.Range(minRandomDegree, maxRandomDegree), 0, Random.Range(minRandomDegree, maxRandomDegree));
+        var enemy = Instantiate(_enemy, _position, Quaternion.identity); //Quaternion.LookRotation(direction));
         enemy.GetComponent<Renderer>().material = _material;
+        enemy.transform.rotation = GetComponent<Mover>().SetDirection(direction);
     }
 
     private IEnumerator Countdown(WaitForSeconds wait)
