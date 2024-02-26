2fa72768e53ca97203ce77d1ee55db5cfcc715eb
Porton12345 <vkruk123@mail.ru>
2024-02-26 13:42:21 +0300

    Six commit

--- My project (23)/Assets/Scripts/Spawner.cs
+++ My project (23)/Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -7,32 +8,34 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private GameObject _enemyCapsule;
     [SerializeField, Min(0)] private float _countSecondsBeforeSpawp;
+    [SerializeField] private Material _material;
 
     private Vector3 _position;
     private int minRandom = 0;
-    private int maxRandom = 5;    
+    private int maxRandom = 5;
     private int _currentSpawnpoint = 0;
-         
 
     private void Start()
     {
-        StartCoroutine(Countdown(_countSecondsBeforeSpawp));        
-    }    
-    
+        var wait = new WaitForSeconds(_countSecondsBeforeSpawp);
+        StartCoroutine(Countdown(wait));
+    }
+
     private void CreateWithInstantiate()
     {
         _currentSpawnpoint = Random.Range(minRandom, maxRandom);
         _position = _spawnPoints[_currentSpawnpoint].position;
         Vector3 direction = new Vector3(Random.Range(minRandom, maxRandom), 0, Random.Range(minRandom, maxRandom));
-        var enemy = Instantiate(_enemyCapsule, _position, Quaternion.LookRotation(_direction));       
-    }    
+        var enemy = Instantiate(_enemyCapsule, _position, Quaternion.LookRotation(_direction));
+        enemy.GetComponent<Renderer>().material = _material;
+    }
 
-    private IEnumerator Countdown(float delay)
+    private IEnumerator Countdown(WaitForSeconds wait)
     {
         while (true)
         {
             CreateWithInstantiate();
-            yield return new WaitForSeconds(delay);
-        }              
-    }    
+            yield return wait;
+        }
+    }
 }
